48446808c08caadb46365ecb2a86a632a7c2878f
mdwisu <dwisusanto784@gmail.com>
2022-11-15 17:24:36 +0700

    menambahkan validasi

--- models/user.js
+++ models/user.js
@@ -3,23 +3,31 @@ const { default: mongoose, Schema } = require('mongoose');
 const userSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Why no bacon?'],
+    required: [true, 'name is required'],
   },
   email: {
     type: String,
-    required: [true, 'Why no bacon?'],
+    trim: true,
     lowercase: true,
+    unique: true,
+    required: [true, 'email is required'],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
   },
   password: {
     type: String,
-    required: [true, 'Why no bacon?'],
-    // trim: true,
-    // minLength: 4,
-    // maxLength: 20,
+    required: [true, 'password is required'],
+    trim: true,
+    minLength: 4,
+    // maxLength: 16,
+    match: [/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,1024}$/, 'password mush have capital, special, and number']
   },
   role: {
     type: String,
-    required: [true, 'Why no bacon?'],
+    enum: ['user', 'admin'],
+    required: [true, 'role is required'],
   },
 });
 
