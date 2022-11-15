98183b6ddab29da838abca94da5efd5c062cb519
mdwisu <dwisusanto784@gmail.com>
2022-11-15 17:25:53 +0700

    mengubah git ignore

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers, getUserById, addUser } = require('../controllers/user.controller');
+const { getUsers, getUserById, addUser, updateUser, deleteUser } = require('../controllers/user.controller');
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', addUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
