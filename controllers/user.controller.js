b3b524f0d5dfb8838108da8737de8612b39e9ed2
mdwisu <dwisusanto784@gmail.com>
2022-11-15 14:06:03 +0700

    create get user by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,20 @@
 const User = require('../models/user');
 
 module.exports = {
-  getUsers: async(req, res) => {
+  getUsers: async (req, res) => {
     try {
       const users = await User.find();
-      res.json(users)
+      res.json(users);
     } catch (error) {
-      res.status(500).json({message: error.message})
+      res.status(500).json({ message: error.message });
+    }
+  },
+  getUserById: async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.json(user);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
     }
   },
 };
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers } = require('../controllers/user.controller');
+const { getUsers, getUserById } = require('../controllers/user.controller');
 
 router.get('/', getUsers);
+router.get('/:id', getUserById);
 
 module.exports = router;
