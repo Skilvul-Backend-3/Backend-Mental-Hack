2a345dcedaa8eecb0e1e310f756cf2d6d973ad7d
mdwisu <dwisusanto784@gmail.com>
2022-11-15 15:38:11 +0700

    create model update and delete

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,11 +17,31 @@ module.exports = {
       res.status(404).json({ message: error.message });
     }
   },
+
   addUser: async (req, res) => {
     const user = new User(req.body);
     try {
       const inserteduser = await user.save();
       res.status(201).json(inserteduser);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  updateUser: async (req, res) => {
+    try {
+      const updateduser = await User.updateOne(
+        { _id: req.params.id },
+        { $set: req.body }
+      );
+      res.status(200).json(updateduser);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  },
+  deleteUser: async (req, res) => {
+    try {
+      const deleteduser = await User.deleteOne({ _id: req.params.id });
+      res.status(200).json(deleteduser);
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
