6881f6f663ae75c5e03cfb97cd703a56ebde3bca
mdwisu <dwisusanto784@gmail.com>
2022-11-20 16:17:01 +0700

    ubah dikit

--- routes/user.router.js
+++ routes/user.router.js
@@ -12,6 +12,7 @@ const { verifyToken, adminOnly } = require('../middleware/authUser');
 
 router.get('/', verifyToken, adminOnly, getUsers);
 // router.get('/', getUsers);
+router.get('/:id', getUserById);
 router.post('/', addUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
